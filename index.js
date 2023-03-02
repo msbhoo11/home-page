7f3021eba8c5b5be2848545d7b320aa875af7159
Bhoomika <bhoomikams11@gmail.com>
2023-03-02 12:38:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $(".and").click(function(){
+        alert("For Android.");
+    });      
+});
+$(document).ready(function(){
+    $(".app").click(function(){
+        alert("For Apple.");
+    });      
+});
